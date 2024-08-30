51ba4a52cbff5ab32cbcee8ce30dbc5a7fc304e2
Fernando Chiñas <A00832747@tec.mx>
2024-08-29 22:39:42 -0600

    Initial Commit

---
+++ SKILIKET_Team7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SKILIKET_Team7
+//
+//  Created by Fernando Chiñas on 29/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
