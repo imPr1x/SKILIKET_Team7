1c49c6588ffdcbc35b0b5438b491e46baca6f0f3
Fernando Chiñas <A00832747@tec.mx>
2024-09-02 06:59:53 -0600

    building app part 1

---
+++ SKILIKET_Team7/2LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  2LoginViewController.swift
+//  SKILIKET_Team7
+//
+//  Created by Fernando Chiñas on 31/08/24.
+//
+
+import UIKit
+
+class _LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SKILIKET_Team7/successRegisteredUserViewController.swift
@@ -0,0 +1,29 @@
+//
+//  successRegisteredUserViewController.swift
+//  SKILIKET_Team7
+//
+//  Created by Fernando Chiñas on 01/09/24.
+//
+
+import UIKit
+
+class successRegisteredUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
