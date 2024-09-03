389b3d39e75aa06302a471166153a5904bf510df
Fernando Chiñas <A00832747@tec.mx>
2024-09-02 20:49:40 -0600

    pretotipo finished

---
+++ SKILIKET_Team7/ProjectUserViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProjectUserViewController.swift
+//  SKILIKET_Team7
+//
+//  Created by Fernando Chiñas on 02/09/24.
+//
+
+import UIKit
+
+class ProjectUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
